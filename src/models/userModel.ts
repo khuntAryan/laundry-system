5b02e39984461d2f4d3fb70f4090e44da7c89e84
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-11-02 00:37:49 +0530

    User Models

---
+++ src/models/userModel.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contactNumber: {
+    type: String,
+    required: true,
+  },
+  hostel: {
+    type: String,
+    enum: ['Mano', 'Mulai', 'Paari'],
+    required: true,
+  },
+  submissionDate: {
+    type: Date,
+    default: null,
+  },
+  expectedRetrievalDate: {
+    type: Date,
+    default: null,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'In Process', 'Completed', 'Ready for Pickup'],
+    default: 'Pending',
+  },
+});
+
+export const user = mongoose.models.User ||mongoose.model('User',userSchema)
